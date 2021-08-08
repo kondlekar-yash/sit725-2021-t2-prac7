b858a5d95441bf86d662db5c73acfe9fbf84640b
Yash Kondlekar <83708020+kondlekar-yash@users.noreply.github.com>
2021-08-08 23:05:06 +0530

    -Connected mongo -created backend for adding and displaying clients

--- server.js
+++ server.js
@@ -2,12 +2,62 @@ var express = require("express")
 var app = express()
 var port = process.env.port || 8080;
 
+const MongoClient = require('mongodb').MongoClient;
+let clientCollection;
+const uri = "mongodb+srv://725:13111995@725week4.ghoce.mongodb.net/725Portfolio?retryWrites=true&w=majority"
+const client = new MongoClient(uri, {useNewUrlParser: true})
+
 var users = [];
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use(express.static(__dirname+'/public'))
+
+const createCollection = (collectionName) => {
+    client.connect ((err,db) => {
+        clientCollection = client.db().collection(collectionName);
+        if(!err) {
+            console.log("MongoDB connected.");
+        }
+        else {
+            console.log("MongoDB error.", err);
+            process.exit(1);
+        }
+    })
+}
+
+const insertClients = (client,callback) => {
+    clientCollection.insert(client,callback);
+}
+
+const getClients = (callback) => {
+    clientCollection.find({}).toArray(callback);
+}
+
+app.get('/api/clients',(req,res) => {
+    getclients((err,resullt) => {
+        if(err) {
+            res.json({statusCode: 400, message:err})
+        }
+        else {
+            res.json({statusCode:200, message:"success",data:result})
+        }
+    })
+})
+
+app.post('/api/clients', (req,res) => {
+    console.log("New client added, please wait for response on mail",req.body)
+    var newClient = req.body;
+    insertClients(newClient,(err,result) => {
+        if(err) {
+            res.json({statusCode:400 , message: err})
+        }
+        else {
+            res.json({statusCode:200, message:"Client added.", data : result})
+        }
+    })
+})
 
 //Created couple of APIs which can be put to use in future.
  
@@ -32,4 +82,5 @@ function showProjects(){
 
 app.listen(port,()=>{
     console.log("App listening to: "+port)
+    createCollection("Clients");
 })
